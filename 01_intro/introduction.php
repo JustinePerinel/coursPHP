6636b06f7304802f75f1206362e6d9da49d23de2
Justine Périnel <justine.perinel@free.Fr>
2021-03-04 15:00:58 +0100

    maj

---
+++ 01_intro/introduction.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <!-- font google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
+
+    <title>Cours PHP7 - Introduction</title>
+
+    <!-- mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <div class="jumbotron">
+        <h1 class="display-3">Cours PHP</h1>
+        <p class="lead">PHP signifie aujourd'hui PHP HYpertext Preprocessor</p>
+        <hr class="my-2">
+        <p>More info</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
+    </div>
+    <div class="row">
+        <div class="col-3">
+        
+        </div>
+        <!-- ============================================================== -->
+        <!-- Contenu principal -->
+        <!-- ============================================================== -->
+        <div class="col-9">
+            <main class="container">
+            
+            
+            
+            </main>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
